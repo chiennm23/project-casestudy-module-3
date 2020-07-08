af7dab557c15484b448450de233443eb9f4bb236
manh chien <chiennm23@gmail.com>
2020-07-08 10:11:50 +0700

    update

--- resources/views/bookings/booking.blade.php
+++ resources/views/bookings/booking.blade.php
@@ -74,36 +74,36 @@
 
 
                                         <!-- Modal -->
-                                        <div class="modal modal-right fade" id="modal-customer" tabindex="-1">
-                                            <div class="modal-dialog" style="width: 50%">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title">Tìm khách hàng</h5>
-                                                        <button type="button" class="close"
-                                                                data-dismiss="modal">
-                                                            <span aria-hidden="true">&times;</span>
-                                                        </button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <div class="box-header with-border bg-light">
-                                                            <input aria-controls="filterCustomer"
-                                                                   class="form-control form-control-sm"
-                                                                   placeholder="Nhập tên hoặc mã để tìm"
-                                                                   type="text" id="search-customer" name="search" width="100%">
-                                                        </div>
-                                                        <div class="col-12 cusomers-list">
-                                                            <div class="box-body">
-                                                                <div class="table-responsive">
-                                                                    <table class="table table-hover mb-0">
-                                                                        <thead>
-                                                                        <tr>
-                                                                            <th scope="col">#</th>
-                                                                            <th scope="col">Name</th>
-                                                                            <th scope="col">Phone</th>
-                                                                            <th></th>
-                                                                        </tr>
-                                                                        </thead>
-                                                                        <tbody id="customer-table">
+{{--                                        <div class="modal modal-right fade" id="modal-customer" tabindex="-1">--}}
+{{--                                            <div class="modal-dialog" style="width: 50%">--}}
+{{--                                                <div class="modal-content">--}}
+{{--                                                    <div class="modal-header">--}}
+{{--                                                        <h5 class="modal-title">Tìm khách hàng</h5>--}}
+{{--                                                        <button type="button" class="close"--}}
+{{--                                                                data-dismiss="modal">--}}
+{{--                                                            <span aria-hidden="true">&times;</span>--}}
+{{--                                                        </button>--}}
+{{--                                                    </div>--}}
+{{--                                                    <div class="modal-body">--}}
+{{--                                                        <div class="box-header with-border bg-light">--}}
+{{--                                                            <input aria-controls="filterCustomer"--}}
+{{--                                                                   class="form-control form-control-sm"--}}
+{{--                                                                   placeholder="Nhập tên hoặc mã để tìm"--}}
+{{--                                                                   type="text" id="search-customer" name="search" width="100%">--}}
+{{--                                                        </div>--}}
+{{--                                                        <div class="col-12 cusomers-list">--}}
+{{--                                                            <div class="box-body">--}}
+{{--                                                                <div class="table-responsive">--}}
+{{--                                                                    <table class="table table-hover mb-0">--}}
+{{--                                                                        <thead>--}}
+{{--                                                                        <tr>--}}
+{{--                                                                            <th scope="col">#</th>--}}
+{{--                                                                            <th scope="col">Name</th>--}}
+{{--                                                                            <th scope="col">Phone</th>--}}
+{{--                                                                            <th></th>--}}
+{{--                                                                        </tr>--}}
+{{--                                                                        </thead>--}}
+{{--                                                                        <tbody id="customer-table">--}}
 {{--                                                                        @forelse($customers as $key => $customer)--}}
 {{--                                                                            <tr>--}}
 {{--                                                                                <th scope="row">{{ ++$key }}</th>--}}
@@ -120,24 +120,24 @@
 {{--                                                                                <td colspan="2">No data</td>--}}
 {{--                                                                            </tr>--}}
 {{--                                                                        @endforelse--}}
-                                                                        </tbody>
-                                                                    </table>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="modal-footer modal-footer-uniform">
-                                                        <button type="button" class="btn btn-bold btn-secondary"
-                                                                data-dismiss="modal">Close
-                                                        </button>
-                                                        <button type="button"
-                                                                class="btn btn-bold btn-primary float-right">
-                                                            Save changes
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+{{--                                                                        </tbody>--}}
+{{--                                                                    </table>--}}
+{{--                                                                </div>--}}
+{{--                                                            </div>--}}
+{{--                                                        </div>--}}
+{{--                                                    </div>--}}
+{{--                                                    <div class="modal-footer modal-footer-uniform">--}}
+{{--                                                        <button type="button" class="btn btn-bold btn-secondary"--}}
+{{--                                                                data-dismiss="modal">Close--}}
+{{--                                                        </button>--}}
+{{--                                                        <button type="button"--}}
+{{--                                                                class="btn btn-bold btn-primary float-right">--}}
+{{--                                                            Save changes--}}
+{{--                                                        </button>--}}
+{{--                                                    </div>--}}
+{{--                                                </div>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
                                         <!-- /.modal -->
                                     </div>
                                     <div class="col-12 col-md-6 book-borrow">
