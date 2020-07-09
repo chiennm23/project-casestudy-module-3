98d360f541ce8bc6d89f50b5d8b1647a82764b12
thang061092 <buimanhthangtb@gmail.com>
2020-07-09 15:51:07 +0700

    make it better

--- app/Http/Requests/ValidateCustomerRequest.php
+++ app/Http/Requests/ValidateCustomerRequest.php
@@ -33,7 +33,7 @@ class ValidateCustomerRequest extends FormRequest
         }
         return [
             'name' => 'required|max:18',
-            'card' => 'required|min:9|max:12|' . $ruleCustomer[0],
+            'card' => 'required|min:9|max:10|' . $ruleCustomer[0],
             'phone' => 'required|' . $ruleCustomer[1]
         ];
     }
@@ -44,8 +44,8 @@ class ValidateCustomerRequest extends FormRequest
             'name.required' => 'Tên không được để trống',
             'name.max' => 'số kí tự nằm trong khoảng 6-18',
             'card.required' => 'CMND không được để trống',
-            'card.min' => 'Số kí tự nằm trong khoảng 9-12',
-            'card.max' => 'Số kí tự nằm trong khoảng 9-12',
+            'card.min' => 'Số kí tự nằm trong khoảng 9-10',
+            'card.max' => 'Số kí tự nằm trong khoảng 9-10',
             'card.unique' => 'CMND đã tồn tại',
             'phone.required' => 'số điện thoại không được để trống',
             'phone.unique' => 'Số điện thoại đã tồn tại'
